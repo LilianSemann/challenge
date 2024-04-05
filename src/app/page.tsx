81a193e8757f2b944a98fd75aa8517544e4b5745
Lilian Cristine Semann <126900345+LilianSemann@users.noreply.github.com>
2024-04-05 17:39:29 -0300

    cume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,73 +2,34 @@ import Footer from "./components/footer"
 import Banner from "./components/banner"
 
 export default function Home() {
+  
   return (
     <div className="h-screen">
+      <audio autoPlay src="/cume.mp3"/>
       <Banner/>
       <div className="z-50 h-fit flex justify-center align-center pb-14 bg-gray-200">
-        <div className="relative justify-center w-[95%] flex flex-col  mt-80 bg-white rounded-lg shadow-lg shadow-gray-300">
-          <p className="p-10">
-            No alto daquele cume
-            Plantei uma roseira
-            O vento no cume bate
-            A rosa no cume cheira.
-
-            Quando cai a chuva fina
-            Salpicos no cume caem
-            Formigas no cume entram
-            Abelhas do cume saem.
-
-            Quanto cai a chuva grossa
-            A água do cume desce
-            O barro do cume escorre
-            O mato no cume cresce.
-
-            Quando cessa a chuva
-            No cume volta a alegria
-            Pois torna a brilhar de novo
-            O sol que no cume ardia!
+        <div className="relative justify-center w-[95%] flex mt-80 bg-white rounded-lg shadow-lg shadow-gray-300 p-10">
+          <p className="pr-32 border-r border-r-zinc-800">
+            No alto daquele cume <br />
+            Plantei uma roseira <br />
+            O vento no cume bate <br />
+            A rosa no cume cheira. <br />
+            <br />
+            Quando cai a chuva fina <br />
+            Salpicos no cume caem <br />
+            Formigas no cume entram <br />
+            Abelhas do cume saem. <br />
           </p>
-          <p className="p-10">
-            No alto daquele cume
-            Plantei uma roseira
-            O vento no cume bate
-            A rosa no cume cheira.
-
-            Quando cai a chuva fina
-            Salpicos no cume caem
-            Formigas no cume entram
-            Abelhas do cume saem.
-
-            Quanto cai a chuva grossa
-            A água do cume desce
-            O barro do cume escorre
-            O mato no cume cresce.
-
-            Quando cessa a chuva
-            No cume volta a alegria
-            Pois torna a brilhar de novo
-            O sol que no cume ardia!
-          </p>
-          <p className="p-10">
-            No alto daquele cume
-            Plantei uma roseira
-            O vento no cume bate
-            A rosa no cume cheira.
-
-            Quando cai a chuva fina
-            Salpicos no cume caem
-            Formigas no cume entram
-            Abelhas do cume saem.
-
-            Quanto cai a chuva grossa
-            A água do cume desce
-            O barro do cume escorre
-            O mato no cume cresce.
-
-            Quando cessa a chuva
-            No cume volta a alegria
-            Pois torna a brilhar de novo
-            O sol que no cume ardia!
+          <p className="pl-32">
+            Quanto cai a chuva grossa <br />
+            A água do cume desce <br />
+            O barro do cume escorre <br />
+            O mato no cume cresce. <br />
+            <br />
+            Quando cessa a chuva <br />
+            No cume volta a alegria <br />
+            Pois torna a brilhar de novo <br />
+            O sol que no cume ardia! <br />
           </p>
         </div>
       </div>
