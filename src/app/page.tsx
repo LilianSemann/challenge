4249e067a7303b96367e016bf716e44430605c02
Lilian Cristine Semann <126900345+LilianSemann@users.noreply.github.com>
2024-05-02 17:08:19 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <Banner/>
       <div className="z-50 h-fit flex justify-center align-center pb-14 bg-gray-200">
         <div className="relative justify-center w-[95%] flex mt-80 bg-white rounded-lg shadow-lg shadow-gray-300 p-10">
-          <p className="pr-32 border-r border-r-zinc-800">
+          <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
           </p>
         </div>
