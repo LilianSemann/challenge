4c88190472792c2971ca625585fa609c84be8157
Lilian Cristine Semann <126900345+LilianSemann@users.noreply.github.com>
2024-05-02 17:06:29 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,26 +10,7 @@ export default function Home() {
       <div className="z-50 h-fit flex justify-center align-center pb-14 bg-gray-200">
         <div className="relative justify-center w-[95%] flex mt-80 bg-white rounded-lg shadow-lg shadow-gray-300 p-10">
           <p className="pr-32 border-r border-r-zinc-800">
-            No alto daquele cume <br />
-            Plantei uma roseira <br />
-            O vento no cume bate <br />
-            A rosa no cume cheira. <br />
-            <br />
-            Quando cai a chuva fina <br />
-            Salpicos no cume caem <br />
-            Formigas no cume entram <br />
-            Abelhas do cume saem. <br />
-          </p>
-          <p className="pl-32">
-            Quanto cai a chuva grossa <br />
-            A água do cume desce <br />
-            O barro do cume escorre <br />
-            O mato no cume cresce. <br />
-            <br />
-            Quando cessa a chuva <br />
-            No cume volta a alegria <br />
-            Pois torna a brilhar de novo <br />
-            O sol que no cume ardia! <br />
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
           </p>
         </div>
       </div>
